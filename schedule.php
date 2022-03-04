6604a088ee521bdaf32f295bdf411b6d21841aae
MaribelHearn <casbas.sj@gmail.com>
2022-03-04 15:51:53 +0100

    Fix credits server error

--- schedule.php
+++ schedule.php
@@ -21,7 +21,7 @@
 <body>
 	<header class="header">
 	<a href="/<?php echo query_string() ?>" class="logo">Touhou World Cup</a>
-	<input class="menu-btn" type="checkbox" id="menu-btn" />
+	<input class="menu-btn" type="checkbox" id="menu-btn">
 	<label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
 	<ul class="menu">
 		<li><a class="subpage" href="rules<?php echo query_string() ?>"><?php echo _('Rules') ?></a></li>
@@ -36,7 +36,7 @@
             }
         ?>
 	</ul>
-    <ul class="menu">
+    <ul class="languages">
         <li<?php echo $lang == 'en_GB' ? ' class="selected"' : '' ?>><a id="en_GB" class="language" href="?hl=en-gb">
             <img src="/assets/uk.png" alt="<?php echo _('Flag of the United Kingdom') ?>" title="English (UK)">
         </a></li>
