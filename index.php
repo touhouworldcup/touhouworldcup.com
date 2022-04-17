247eac9e2c0d9d6ae08cbcca6d15688fc2f8bfb8
Maribel Hearn <maribelhearn@disroot.org>
2022-04-17 18:22:58 +0200

    TWC 2022

--- index.php
+++ index.php
@@ -8,29 +8,40 @@
 
 <body>
 	<?php include_once 'php/body.php' ?>
-	<main>
+    <main>
 	<h1>Touhou World Cup</h1>
     <p><?php
         echo _('<strong>Touhou World Cup (TWC)</strong> is an annual international Touhou shooting game competition, ' .
-        'first held in 2020, organised by the Chinese gameplay community. ' .
-        'This time around, in 2021, it is organised by the Western gameplay community.');
+        'first held in 2020 and held every year since. It is organised by the Touhou gameplay community worldwide.');
     ?></p>
     <p><?php
-        echo _('Three teams, a Western community team, a Chinese team, and a Japanese team, duke it out ' .
-        'on Lunatic and Extra mode, playing on live streams and playing both high score and survival.');
+        echo _('Three teams duke it out on Lunatic and Extra mode, playing on live streams and playing both high score and survival.');
     ?></p>
     <p><?php
-        echo _('See below for official communication channels and live streams with commentary.');
+        echo _('See below for official communication channels.');
     ?></p>
-	<ul class="list">
-		<li><a href="https://twitter.com/touhouworldcup"><?php echo _('Official TWC Twitter') ?></a></li>
-		<li><a href="https://www.youtube.com/channel/UCk8o-jk-Zpn4CEmLUIkui0A"><?php echo _('Official TWC YouTube Channel') ?></a></li>
-		<li><a href="https://www.twitch.tv/touhou_replay_showcase"><?php echo _('English commentary stream (Twitch)') ?></a></li>
-		<li><a href="https://live.bilibili.com/22478102?share_source=copy_link"><?php echo _('Chinese commentary stream 1 (Bilibili)') ?></a></li>
-		<li><a href="https://live.bilibili.com/14315174?share_source=copy_link"><?php echo _('Chinese commentary stream 2 (Bilibili)') ?></a></li>
-		<li><a href="https://www.youtube.com/channel/UCfF3O4wo0YxppTZGmtTGDwg"><?php echo _('Japanese commentary stream (YouTube)') ?></a></li>
-		<li><a class="dead" href="https://www.twitch.tv/touhou_russian_kolkhoz"><?php echo _('Russian commentary stream (Twitch)') ?></a></li>
-	</ul>
+    <div id="countdowns">
+        <h2><?php echo _('TWC Starts In:') ?></h2>
+        <p class="large"><span id="countdown_start"></span></p>
+    </div>
+    <div id="cards">
+        <div class="card"><a href="https://www.twitch.tv/touhou_replay_showcase" class="card_link" target="_blank">
+            <img class="thumbnail" src="/assets/twitch.png" alt="TWC Twitch stream" width=500 height=320>
+            <span class="title large"><img class="icon" src="/assets/icons/twitch-icon.png" alt="Twitch icon" width=64 height=64> <?php echo _('TWC English Commentary Stream') ?></span>
+        </a></div>
+        <div class="card"><a href="https://www.youtube.com/c/TouhouWorldCup" class="card_link" target="_blank">
+            <img class="thumbnail" src="/assets/youtube.png" alt="TWC YouTube channel" width=500 height=320>
+            <span class="title large"><img class="icon" src="/assets/icons/youtube-icon.png" alt="YouTube icon" width=64 height=64> <?php echo _('Official TWC YouTube Channel') ?></span>
+        </a></div>
+        <div class="card"><a href="https://twitter.com/touhouworldcup" class="card_link" target="_blank">
+            <img class="thumbnail" src="/assets/twitter.png" alt="TWC Twitter account" width=500 height=320>
+            <span class="title large"><img class="icon" src="/assets/icons/twitter-icon.png" alt="Twitter icon" width=64 height=64> <?php echo _('Official TWC Twitter') ?></span>
+        </a></div>
+    </div>
+    <!--
+    <a class="twitter-timeline" data-width="300" data-height="500" data-dnt="true" data-theme="light" href="https://twitter.com/touhouworldcup?ref_src=twsrc%5Etfw">Tweets by touhouworldcup</a>
+    -->
+    <p class="bottom">Touhou World Cup 2022</p>
 	</main>
 </body>
 </html>
