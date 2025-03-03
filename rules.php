75c921e63fa8193a18c263a20659807b3c36d363
32th-System <intensonat@gmail.com>
2025-03-03 11:27:36 +0100

    Update rulebook

--- rules.php
+++ rules.php
@@ -13,7 +13,7 @@
     <h2><?php echo _('Full Official Rules') ?></h2>
     <p><?php echo _('The full official rulebook can be found here:') ?></p>
     <ul class="style-none">
-        <li>English: <a href="https://docs.google.com/document/d/e/2PACX-1vTATjRLhS3tTXoyp1NtoSVVsehDgVm22QZxBOCCnrXWVpzycXxz6Y4NDh7rX850ihByJ3-LUDrL24HU/pub" target="_blank">TWC Handbook</a></li>
+        <li>English: <a href="https://docs.google.com/document/d/e/2PACX-1vQrmgcwhgKARoUnk5BPE0Oyv4fAgHilZs1pUa1RQJtm0X_z93L8eI0lNt1Y-iZQK3v4_Ab9vx1HzpkN/pub" target="_blank">TWC Handbook</a></li>
         <li>日本語: <a href="assets/TWC_Handbook_JP.pdf" target="_blank">TWC_Handbook_JP.pdf</a></li>
     </ul>
     <hr>
