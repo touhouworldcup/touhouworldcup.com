2b5ece49e38caa9d44fc5a6e5b23b069ac004e89
DESKTOP-K07LRU0\SOC <tss428x@gmail.com>
2025-05-19 04:04:31 +0900

    remove Remove TWC2025 announcement from index

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
         <h2 id="countdown_title_match"><?php echo _('Next Match:') ?></h2>
         <p id="countdown" class="large"><span id="countdown_start"></span></p>
         <h2 id="current_match"><?php echo _('Currently ongoing: <a class="match_link" href="https://twitch.tv/touhou_replay_showcase" target="_blank">')?><span id="match_category"></span></a></h2>
-        <p class="huge"><?php echo _('Touhou World Cup 2025 will start on May 17th.') ?></p>
+        <!-- <p class="huge"><?php echo _('Touhou World Cup 2025 will start on May 17th.') ?></p> -->
     </div>
     <div id="cards">
         <div class="cards_inner">
